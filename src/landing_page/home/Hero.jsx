d45c0400462628f20cc6e1416e6735b95148cc27
Anay  Patil <anaypatil@Anays-MacBook-Air.local>
2025-07-07 18:12:04 +0530

    Complete Hero Section of Home Page

--- src/landing_page/home/Hero.jsx
+++ src/landing_page/home/Hero.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 function Hero() {
     return ( 
-        <h1> Hero </h1>
+        <div className='container p-5'>
+            <div className='row text-center'>
+                <div className='col'>
+                    <img src="media/images/homeHero.png" alt="Hero Image" width={1000} height={430} className='mb-5' />
+                </div>
+                <h1 className='mt-5'> Invest in everything </h1>
+                <p className='mt-1'>
+                    Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.
+                </p>
+                <button className='mt-3 p-2 btn btn-primary fs-5' style={{width: '20%', margin: '0 auto'}}>
+                    Signup Now
+                </button>
+            </div>
+        </div>
      );
 }
 
