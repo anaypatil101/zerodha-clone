fa50ee92a24952f9229f5b5dbbec5c01bb3f4f66
Anay  Patil <anaypatil@Anays-MacBook-Air.local>
2025-07-07 16:33:45 +0530

    Initial Commit

---
+++ src/landing_page/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+    return ( 
+        <h1> Footer <i class="fa fa-free-code-camp" aria-hidden="true"></i> </h1>
+     );
+}
+
+export default Footer;
