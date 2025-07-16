a785d079686a6e8ca96a9b6d23b2b75144b75d4c
Anay  Patil <anaypatil@Anays-MacBook-Air.local>
2025-07-16 12:35:01 +0530

    Complete Home Module

--- src/landing_page/home/Hero.jsx
+++ src/landing_page/home/Hero.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 function Hero() {
     return ( 
-        <div className='container p-5'>
+        <div className='container p-5 mb-5'>
             <div className='row text-center'>
                 <div className='col'>
                     <img src="media/images/homeHero.png" alt="Hero Image" width={1000} height={430} className='mb-5' />
                 </div>
                 <h1 className='mt-5'> Invest in everything </h1>
-                <p className='mt-1'>
+                <p className='mt-1 fs-5'>
                     Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.
                 </p>
-                <button className='mt-3 p-2 btn btn-primary fs-5' style={{width: '20%', margin: '0 auto'}}>
+                <button className='mt-3 p-2 btn btn-primary fs-5 mb-3' style={{width: '20%', margin: '0 auto'}}>
                     Signup Now
                 </button>
             </div>
